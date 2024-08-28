9897cc6cdf14193e401aa80e70e729555a55faa8
ft <fetianans@gmail.com>
2024-08-29 00:08:00 +0300

    maiz site

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,27 @@
+
+export default function Footer() {
+  return (
+    <>
+    <div className="footer">
+        <h2>Maiz</h2>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Events</li>
+            <li>Contactus</li>
+        </ul>
+
+        <ul>
+            <li>facebook</li>
+            <li>twitter</li>
+            <li>linkedin</li>
+        </ul>
+
+        <p>Copyright</p>
+
+    </div>
+    
+    </>
+  )
+}
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,50 @@
+export default function Home() {
+  return (
+    <>
+      <div>
+        <h2>Taste the authentic Saudi cuisine</h2>
+        <p>Among the best Saudi chefs in the world.</p>
+        <p>serving you something beyound flower</p>
+        <img src="eat" alt="" />
+
+        <button>Our Menue</button>
+      </div>
+      <div>
+        <img src="home" alt="" />
+        <h2>A distinctive dining destination inspired by the culture.</h2>
+        <p>Experince AI Balad old town of Jadden vibes.</p>
+
+        <button>Our Venue </button>
+
+        <ul>
+          <li>
+            <img src="img1" alt="" />
+          </li>
+          <li>
+            <img src="img2" alt="" />
+          </li>
+          <li>
+            <img src="img3" alt="" />
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <p>You cant go wrong with Chicken Mandi, I had it twice. The chicken was cooked perfectly, juicy & soft (usually mandi chicken is a bit dry). I would defiantly recommend it.</p>
+        <p>Name: jamal jamal</p>
+        <img src="face" alt="" />
+      </div>
+      <div>
+        <img src="video" alt="" />
+      </div>
+
+      <div className="bannar">
+        <img src="bannar" alt="" />
+        <h2>
+          A unique menue that reflects the true essence of the Saudi cusine
+        </h2>
+        <button>Our Menue</button>
+      </div>
+    </>
+  );
+}
